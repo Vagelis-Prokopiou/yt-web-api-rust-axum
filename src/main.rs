8fd7192343e039cfbfb8ebb0f3836e6900450ff6
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-26 11:42:19 +0200

    Adding num_cpus::get_physical()

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ use api_axum::get_users;
 fn main() {
     let runtime = Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(8)
+        .worker_threads(num_cpus::get_physical())
         .build()
         .unwrap();
 
     // Execute the future, blocking the current thread until completion
     runtime.block_on(async {
         let app = Router::new().route("/users", get(get_users));
+
+        println!("\nServer running at: http://0.0.0.0:3000/users");
 
         // run it with hyper on localhost:3000
         axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
