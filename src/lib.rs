dbcb351bd3205c976045217b0a3b3ef8b58c87d0
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-26 11:23:17 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use axum::Json;
+
+use crate::models::User;
+
+pub mod models;
+
+pub async fn get_users() -> Json<Vec<User>> {
+    let mut users = Vec::with_capacity(1000);
+    for index in 1..1001_u16 {
+        users.push(User {
+            Id: index,
+            Age: 25,
+            First_Name: format!("First_Name{}", index),
+            Last_Name: format!("Last_Name{}", index),
+            Framework: "Rust (warp)     ".to_owned(),
+        })
+    }
+    Json(users)
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use axum::routing::get;
+use axum::Router;
+use tokio::runtime::Builder;
+
+use api_axum::get_users;
+
+fn main() {
+    let runtime = Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(8)
+        .build()
+        .unwrap();
+
+    // Execute the future, blocking the current thread until completion
+    runtime.block_on(async {
+        let app = Router::new().route("/users", get(get_users));
+
+        // run it with hyper on localhost:3000
+        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    });
+}
